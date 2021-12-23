7956a4da88b374d891346087e095451e7de11a5f
Piotr-Soltys <95974786+Piotr-Soltys@users.noreply.github.com>
2021-12-23 22:18:50 +0100

    Zadanie 2 Pkt 2a

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace PPK___Zadanie_2_pkt_2
+{
+    internal class Pkt2
+    {
+        static void Main()
+        {
+        StartProgramu:
+            Console.Clear();
+            ulong liczba;
+
+            ulong ObliczSilnie(ulong liczba)
+            {
+                ulong silnia = 1;
+
+                for (ulong i = 1; i <= liczba; i++)
+                {
+                    silnia *= i;
+                }
+                return silnia;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" *** Program pozwalający obliczyć silnię z liczby podanej przez użytkownika ***\n\n");
+            Console.ResetColor();
+            Console.Write(" Podaj liczbę: ");
+            try
+            {
+                liczba = Convert.ToUInt64(Console.ReadLine());
+            }
+            catch
+            {
+                goto StartProgramu;
+            }
+            UInt64 wynik = ObliczSilnie(liczba);
+            if (wynik == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\n Nie można obliczyć silni z liczby {liczba}. Jest poza maksymalną wartością zmiennej.\n");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\n Silnia liczby {liczba} wynosi {ObliczSilnie(liczba)}\n");
+                Console.ResetColor();
+            }
+
+            Console.Read();
+            goto StartProgramu;
+        }
+    }
+}
+
