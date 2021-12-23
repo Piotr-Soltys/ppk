1beb287468b62a1c336a27f306276094de56ccdf
Piotr-Soltys <95974786+Piotr-Soltys@users.noreply.github.com>
2021-12-23 21:34:44 +0100

    Zadanie 2 Pkt 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PPK___Zadanie_2_pkt_1
+{
+    internal class Pkt1
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World");
+
+            Console.Read();
+        }
+    }
+}
+
