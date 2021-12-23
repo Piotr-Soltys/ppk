13ba6cb4cfdb0a896b7ccf6af2330749bcf4d1b3
Piotr-Soltys <95974786+Piotr-Soltys@users.noreply.github.com>
2021-12-23 22:59:32 +0100

    Zadanie 2 Pkt 3

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace PPK___Zadanie_2_pkt_3
+{
+    internal class Pkt3
+    {
+        static void Main()
+        {
+        StartProgramu:
+            Console.Clear();
+            ulong wynik;
+            string liczby;
+
+            ulong Iloczyn(ulong liczba1, ulong liczba2)
+            {
+                return liczba1 * liczba2;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" *** Kalkulator ***\n\n");
+            Console.ResetColor();
+            Console.Write(" Podaj liczby (oddzielone spacją): ");
+            liczby = Console.ReadLine();
+            string [] tablicaString = liczby.Split(' ') ;
+            ulong[] tablicaLiczb = new ulong[3];
+            for (int i = 0; i < tablicaString.Length; i++)
+            {
+                try
+                {
+                    tablicaLiczb[i] = Convert.ToUInt64(tablicaString[i]);
+                }
+                catch
+                {
+                    goto StartProgramu;
+                }
+            }
+            wynik = tablicaLiczb[0] * tablicaLiczb[1];
+            Console.WriteLine($" Iloczyn tych liczb to {wynik}");
+            Console.Read();
+            goto StartProgramu;
+        }
+    }
+}
+
